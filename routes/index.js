6641a8676996e21d189f5f1d001a9780e4b74075
miracletech322 <miracle.tech322@gmail.com>
2024-12-17 00:46:30 +0300

    Auth Middleware

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
+const authMiddleware = require('./../middlewares/auth.middleware');
+
 const authRoute = require('./auth.route');
 const importRoute = require('./import.route');
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
-router.use('/import', importRoute);
+router.use('/import', [authMiddleware.checkAuth], importRoute);
 
 module.exports = router;
