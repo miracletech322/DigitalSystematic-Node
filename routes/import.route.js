99e8ddc85630bbd57c539937d00a8f5f11633131
miracletech322 <miracle.tech322@gmail.com>
2024-12-17 23:44:44 +0300

    Import

--- routes/import.route.js
+++ routes/import.route.js
@@ -3,6 +3,7 @@ const importController = require('../controllers/import.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
 
+router.post('/dashboard', [authMiddleware.checkAuth], importController.dashboardAction);
 router.post('/upload', [authMiddleware.checkAuth], importController.uploadAction);
 
 module.exports = router;
